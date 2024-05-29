b51f1c93bc3b6ccdedb07b7e59b645b2e00c8286
VividTruthKeeper <mr.aydogdy02@gmail.com>
2024-05-29 19:40:13 +0900

    Add viewport configuration to layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,18 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import Loader from "@/components/sections/loader";
 import Provider from "@/config/provider";
+import { Viewport } from "next";
+import { colors } from "@/config/colors";
 import { getSEOTags } from "@/lib/seo";
 
 const font = Inter({ subsets: ["latin"] });
+
+export const viewport: Viewport = {
+  // Will use the primary color of your theme to show a nice theme color in the URL bar of supported browsers
+  themeColor: colors.main,
+  width: "device-width",
+  initialScale: 1,
+};
 
 export const metadata = getSEOTags();
 
